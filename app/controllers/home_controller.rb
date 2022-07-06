4a9be3f66b70ed3505da97a827e9e1b9c30e45f5
mancini-michael <mich.mancini06@gmail.com>
2022-07-06 17:24:22 +0200

    Implement user to seller and seller to user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,20 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def user_to_seller
+    if user_signed_in?
+      seller = Seller.create(user: User.find(params[:id]))
+      respond_to do |format|
+        format.html { redirect_to root_path } if seller.save
+      end
+    end
+  end
+
+  def seller_to_user
+    if user_signed_in?
+      seller = Seller.destroy_by(user_id: params[:id])
+      respond_to { |format| format.html { redirect_to root_path } }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   root "home#index"
   get "home/index"
+  post "/update_user/:id", to: "home#user_to_seller", as: "update_user"
+  post "/restore_user/:id", to: "home#seller_to_user", as: "restore_user"
 end
