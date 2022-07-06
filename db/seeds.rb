e282b9541cf6ba31c1335189427d173919ae05a9
mancini-michael <mich.mancini06@gmail.com>
2022-07-06 16:23:52 +0200

    Create seller model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,30 @@
-User.create(
-  first_name: "Johnny",
-  last_name: "Doe",
-  city: "New York",
-  zip_code: "10001",
-  email: "johnny.doe@gmail.com",
-  password: "password"
-)
-User.create(
-  first_name: "John",
-  last_name: "Doe",
-  city: "New York",
-  zip_code: "10001",
-  email: "john.doe@gmail.com",
-  password: "password"
-)
-User.create(
-  first_name: "Jake",
-  last_name: "Doe",
-  city: "Boston",
-  zip_code: "02108",
-  email: "jake.doe@gmail.com",
-  password: "password"
-)
+user_1 =
+  User.create(
+    first_name: "Johnny",
+    last_name: "Doe",
+    city: "New York",
+    zip_code: "10001",
+    email: "johnny.doe@gmail.com",
+    password: "password"
+  )
+user_2 =
+  User.create(
+    first_name: "John",
+    last_name: "Doe",
+    city: "New York",
+    zip_code: "10001",
+    email: "john.doe@gmail.com",
+    password: "password"
+  )
+user_3 =
+  User.create(
+    first_name: "Jake",
+    last_name: "Doe",
+    city: "Boston",
+    zip_code: "02108",
+    email: "jake.doe@gmail.com",
+    password: "password"
+  )
+
+seller_1 = Seller.create(user: user_2)
+seller_2 = Seller.create(user: user_3)
