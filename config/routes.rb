f159baf875ed6ce1b72e8a6a54294f683d1ee833
mancini-michael <mich.mancini06@gmail.com>
2022-07-07 13:24:28 +0200

    Fix style

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :user, controllers: { registrations: "users/registrations" }
 
   root "home#index"
+
   get "home/index"
   post "/update_user/:id", to: "home#user_to_seller", as: "update_user"
   post "/restore_user/:id", to: "home#seller_to_user", as: "restore_user"
