fc1b5998b49b0ac4e14ec5a4085e7e3463de2e35
mancini-michael <mich.mancini06@gmail.com>
2022-07-07 15:27:08 +0200

    Add geocoder API functions

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,32 @@ class User < ApplicationRecord
          :validatable
 
   has_one :seller
+
+  geocoded_by :address
+  after_validation :geocode
+
+  def address
+    [city, zip_code].join(", ")
+  end
+
+  def location
+    [latitude, longitude]
+  end
+
+  def distance_from(coordinates)
+    distance =
+      Geocoder::Calculations.distance_between(
+        [latitude, longitude],
+        coordinates,
+        units: :km
+      ).to_i
+    return "near to you" if distance <= 10
+    "#{distance} km away"
+  end
+
+  private
+
+  def self.full_name(user)
+    "#{user.first_name} #{user.last_name}"
+  end
 end
