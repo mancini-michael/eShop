5cc10ac36930d765902fc5589665e79b73721a23
mancini-michael <mich.mancini06@gmail.com>
2022-07-07 19:32:14 +0200

    Fix bug in navbar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,16 +7,16 @@ class HomeController < ApplicationController
   end
 
   def user_to_seller
-    if user_signed_in?
-      seller = Seller.create(user: User.find(params[:id]))
-      respond_to do |format|
-        format.html { redirect_to root_path } if seller.save
-      end
+    if user_signed_in? and current_user.id == params[:id].to_i
+      seller = Seller.find_by(user_id: params[:id])
+      seller = Seller.create(user: User.find(params[:id])) if !seller
+      seller.update(active: !seller.active) if seller
+      respond_to { |format| format.html { redirect_to root_path } }
     end
   end
 
   def seller_to_user
-    if user_signed_in?
+    if user_signed_in? and current_user.id == params[:id].to_i
       seller = Seller.find_by(user_id: params[:id])
       seller.update(active: !seller.active)
       respond_to { |format| format.html { redirect_to root_path } }
