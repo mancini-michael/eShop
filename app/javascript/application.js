c92c5cf37db7a3764cae3ae943806a9d948a1303
mancini-michael <mich.mancini06@gmail.com>
2022-07-21 12:34:53 +0200

    Fix style in homepage

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@fortawesome/fontawesome-free";
 import "@hotwired/turbo-rails";
 import "bootstrap";
 import "controllers";
