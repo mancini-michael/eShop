1d060f5f19e2331e2cd117b51be42e21e01f277b
loregi01 <gizzilorenzo@virgilio.it>
2022-08-02 14:50:57 +0200

    fixedLinkQuestions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -57,7 +57,7 @@ class QuestionsController < ApplicationController
     @question.destroy
 
     respond_to do |format|
-      format.html { redirect_to questions_url, notice: "Question was successfully destroyed." }
+      format.html { redirect_to "/insertion/"+$insertion_id.to_s, notice: "Domanda eliminata con successo" }
       format.json { head :no_content }
     end
   end
