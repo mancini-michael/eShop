c2dc5e8337df66c1294a7eb0e05b6ab7520ad060
loregi01 <gizzilorenzo@virgilio.it>
2022-08-05 15:01:16 +0200

    addCreateQuestionTest

---
+++ spec/requests/question_request.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.describe Question, type: :request do
+  describe "POST /question" do
+    it "create a new question" do
+      user = User.create(first_name: "Mario", last_name: "Rossi", city: "Roma", zip_code: "00100", email: "mario.rossi@gmail.com", password: "password")
+      expect(user).to be_valid
+      seller = Seller.create(user: user)
+      expect(seller).to be_valid
+      insertion = Insertion.create(seller: seller, title: "Inserzione #1", description: "Descrizione #1", price: 10, categories: "films")
+      expect(insertion).to be_valid
+      question = Question.create(insertion_id: insertion.id, user_id: user.id, question: "Domanda #1", reply: "")
+      expect(question).to be_valid
+    end
+  end
+end
