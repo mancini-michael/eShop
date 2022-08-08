ff2fcfc7a0414c08c65b8deddc4664219aec7467
mancini-michael <mich.mancini06@gmail.com>
2022-08-08 16:01:34 +0200

    Add unit test for search

--- spec/requests/insertion_request.rb
+++ spec/requests/insertion_request.rb
@@ -11,4 +11,17 @@ RSpec.describe Insertion, type: :request do
       expect(insertion).to be_valid
     end
   end
+  
+  describe "GET /insertion/search" do
+    it "find an insertion" do
+      user = User.create(first_name: "Mario", last_name: "Rossi", city: "Roma", zip_code: "00100", email: "mario.rossi@gmail.com", password: "password")
+      expect(user).to be_valid
+      seller = Seller.create(user: user)
+      expect(seller).to be_valid
+      insertion = Insertion.create(seller: seller, title: "Inserzione #1", description: "Descrizione #1", price: 10, categories: "films")
+      expect(insertion).to be_valid
+      get "/insertion/search", params: { search: "Inserzione" }
+      expect(response).to have_http_status(200)
+    end
+  end
 end
